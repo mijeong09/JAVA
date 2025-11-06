47b033ec9bedad66b33a1a844b3de8301610f1c5
mijeong09 <me9809180@gmail.com>
2025-11-06 15:48:19 +0900

    Package, Access Modifier

---
+++ src/c/oop2/Account.java
@@ -0,0 +1,41 @@
+package c.oop2;
+
+public class Account {
+    private String name;
+    private int age;
+    private String phone;
+    private String address;
+
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
---
+++ src/c/oop2/Package.java
@@ -0,0 +1,14 @@
+package c.oop2;
+
+//import java.util.ArrayList;
+//import java.util.HashMap;
+import java.util.*;
+
+public class Package {
+    public static void main(String[] args) {
+        // array와 유사
+        ArrayList<String> list = new ArrayList<>();
+        // object
+        HashMap<String, Integer> map = new HashMap<>();
+    }
+}
