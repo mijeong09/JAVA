282253f6ce531bc19ee763080420659bf43a95a8
mijeong09 <me9809180@gmail.com>
2025-11-11 09:33:04 +0900

    practice 코드 추가 및 수정

--- src/d/inheritance/practice2/Practice01.java
+++ src/d/inheritance/practice2/Practice01.java
@@ -1,13 +1,13 @@
 package d.inheritance.practice2;
 
 class Animal {
+    String name;
+    int age;
+
     public Animal(String name, int age) {
         this.name = name;
         this.age = age;
     }
-
-    String name;
-    int age;
 
     public void makeSound() {
         System.out.println("소리를 냄");
@@ -49,23 +49,35 @@ class Monkey extends Animal {
 }
 
 class Zoo {
-    Animal[] animal = {
-      new Lion("심바", 10),
-      new Elephant("덤보", 10),
-      new Monkey("조조", 10)
-    };
-    public void feedingTime(Animal animal) {
-        System.out.print(animal.name + " : ");
-        animal.makeSound();
+    Animal[] animals = {};
+    int count;
+
+    public Zoo(int capacity) {
+        this.animals = new Animal[capacity];
+        this.count = 0;
+    }
+
+    public void addAnimal(Animal animal) {
+        if (count < animals.length) {
+            animals[count++] = animal;
+        }
+    }
+
+    public void feedingTime() {
+        for (int i = 0; i < count; i++) {
+            System.out.print(animals[i].name + " : ");
+            animals[i].makeSound();
+        }
     }
 }
 
 public class Practice01 {
     public static void main(String[] args) {
-        Zoo zoo = new Zoo();
+        Zoo zoo = new Zoo(10);
+        zoo.addAnimal(new Lion("심바", 5));
+        zoo.addAnimal(new Elephant("덤보", 10));
+        zoo.addAnimal(new Monkey("우끼", 3));
 
-        for (Animal animal : zoo.animal) {
-            zoo.feedingTime(animal);
-        }
+        zoo.feedingTime();
     }
 }
